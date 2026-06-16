11b74256a0f9f32283f04d49ebf10f593c9f6d91
ImmortalCanary <elif.oflaz@hotmail.com>
2026-06-16 09:26:54 +0200

    Revise documentation for Datenspeicher class
    Updated class documentation to reflect changes in serialization method and added details about lecture topics covered.

--- KuehlschrankManager/src/de/hwrberlin/kuehlschrank/util/Datenspeicher.java
+++ KuehlschrankManager/src/de/hwrberlin/kuehlschrank/util/Datenspeicher.java
@@ -3,12 +3,22 @@ import java.io.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 /**
- * Generische Serialisierungs-Hilfsklasse.
- * Vorlesung 2.1.5: ObjectOutputStream/InputStream (persistente Objekte).
- * Vorlesung 2.1.8: Generische Methoden <T extends Serializable>.
+ * Generische Persistenz-Hilfsklasse auf Basis von JSON (Bibliothek: Google Gson).
+ *
+ * Abgedeckte Vorlesungsthemen:
+ *  - 2.1.8 Generics: generische Methoden mit Typschranke &lt;T extends Serializable&gt;
+ *          (bounded type parameter) und Class&lt;T&gt; als Typ-Token beim Laden.
+ *  - 2.1.6 Exceptions: try-with-resources, FileNotFoundException gesondert behandelt.
+ *  - 2.1.5 Persistenz: Objekte werden in eine Datei geschrieben und wieder gelesen.
+ *
+ * Abweichung von der Vorlesung (bewusst begruendet, siehe REVIEW_Vorlesungsabdeckung.md):
+ * Die Vorlesung 2.1.5 zeigt die Serialisierung ueber ObjectOutputStream/ObjectInputStream.
+ * Hier wird stattdessen JSON (Gson) genutzt -> menschenlesbar und versionsrobuster,
+ * aber externe Bibliothek und nutzt nicht den Serializable-Mechanismus des JDK.
+ * Die JDK-Variante ist zum Vergleich in BinaerSpeicher umgesetzt.
  */
 public class Datenspeicher {
-    private Datenspeicher() {}
+    private Datenspeicher() {}  // Utility-Klasse: keine Instanzen.
     
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     
