1b97268b0b0d5f5e7077739fefd8e98f1537ec84
ImmortalCanary <elif.oflaz@hotmail.com>
2026-07-27 23:29:44 +0200

    refactor: remove German duplicate (replaced by ProductCategory.java)

--- KuehlschrankManager/src/de/hwrberlin/kuehlschrank/model/Produktkategorie.java
+++
@@ -1,6 +0,0 @@
-package de.hwrberlin.kuehlschrank.model;
-/** Enum fuer Produktkategorien. Vorlesung: Typsicherheit. */
-public enum Produktkategorie {
-    LEBENSMITTEL, GETRAENKE, MILCHPRODUKTE,
-    FLEISCH_FISCH, OBST_GEMUESE, MEDIKAMENTE, SONSTIGES
-}
